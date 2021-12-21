0991f4367f57fa9ecb2920aabf0d8263992c75dd
Trần Thuật <71955928+mithuat@users.noreply.github.com>
2021-12-21 12:10:42 +0700

    Thuat Update

--- thuat.php
+++ thuat.php
@@ -1,1 +1,27 @@
-Đây là phần của thuật
+<?php
+    include_once("header.php");
+?>
+    <!-- Begin Header -->
+    
+<?php
+    include_once("thuat.php");
+?>
+    <div class="main px-10 py-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <?php
+                        include_once("truong.php");
+                    ?>
+                </div>
+                <div class="col-4">
+                    <?php
+                        include("sang.php");
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+    include_once("footer.php");
+?>
