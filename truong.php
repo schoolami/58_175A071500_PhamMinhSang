eea7c570d7bdff524cea4d542862d029d6167899
66_175A071514_THANVANTRUONG <94340180+schoolami@users.noreply.github.com>
2021-12-21 14:28:20 +0700

    Truong Update

--- truong.php
+++ truong.php
@@ -1,1 +1,82 @@
-Đây là phần của trường
+<div class="row">
+    <div class="col-10">
+        <h3 class="title">Album mới nhất 2021</h3>
+    </div>
+    <div class="col-2">
+        <a class="float-end my-sub-link" href="#">Xem tất cả</a>
+    </div>
+</div>
+<div class="row">
+    <div class="col-2 col-20">
+        <div class="box pb-5">
+            <div class="box-img">
+                <img src="./assets/images/haiduongmenhdanhlaphithuong.jpg" alt="">
+            </div>
+            <div class="box-content">
+                <a class="txt txt-bold my-link" href="#">Hải Vương Mệnh Danh Là Phi Thường (海王注定不平凡) (Single)</a>
+                <a class="sub-txt my-link" href="#">Lý Ha Ha (李哈哈)</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-2 col-20">
+        <div class="box pb-5">
+            <div class="box-img">
+                <img src="./assets/images/haiduongmenhdanhlaphithuong.jpg" alt="">
+            </div>
+            <div class="box-content">
+                <a class="txt txt-bold my-link" href="#">Hải Vương Mệnh Danh Là Phi Thường (海王注定不平凡) (Single)</a>
+                <a class="sub-txt my-link" href="#">Lý Ha Ha (李哈哈)</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-2 col-20">
+        <div class="box pb-5">
+            <div class="box-img">
+                <img src="./assets/images/haiduongmenhdanhlaphithuong.jpg" alt="">
+            </div>
+            <div class="box-content">
+                <a class="txt txt-bold my-link" href="#">Hải Vương Mệnh Danh Là Phi Thường (海王注定不平凡) (Single)</a>
+                <a class="sub-txt my-link" href="#">Lý Ha Ha (李哈哈)</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-2 col-20">
+        <div class="box pb-5">
+            <div class="box-img">
+                <img src="./assets/images/haiduongmenhdanhlaphithuong.jpg" alt="">
+            </div>
+            <div class="box-content">
+                <a class="txt txt-bold my-link" href="#">Hải Vương Mệnh Danh Là Phi Thường (海王注定不平凡) (Single)</a>
+                <a class="sub-txt my-link" href="#">Lý Ha Ha (李哈哈)</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-2 col-20">
+        <div class="box pb-5">
+            <div class="box-img">
+                <img src="./assets/images/haiduongmenhdanhlaphithuong.jpg" alt="">
+            </div>
+            <div class="box-content">
+                <a class="txt txt-bold my-link" href="#">Hải Vương Mệnh Danh Là Phi Thường (海王注定不平凡) (Single)</a>
+                <a class="sub-txt my-link" href="#">Lý Ha Ha (李哈哈)</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-2 col-20">
+        <div class="box pb-5">
+            <div class="box-img">
+                <img src="./assets/images/haiduongmenhdanhlaphithuong.jpg" alt="">
+            </div>
+            <div class="box-content">
+                <a class="txt txt-bold my-link" href="#">Hải Vương Mệnh Danh Là Phi Thường (海王注定不平凡) (Single)</a>
+                <a class="sub-txt my-link" href="#">Lý Ha Ha (李哈哈)</a>
+            </div>
+        </div>
+    </div>
+    
+</div>
